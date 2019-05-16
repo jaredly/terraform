224d610c8d175005a59310841c548ab95f7edfe6
Jared Forsyth <jared@jaredforsyth.com>
2019-05-16 11:21:07 -0400

    capital t

--- bundle/bundle.sh
+++ bundle/bundle.sh
@@ -1,9 +1,9 @@
 set -ex
 echo Hi
-rm -rf terraform.app
-./macapp.sh terraform icon.png
+rm -rf Terraform.app
+./macapp.sh Terraform icon.png
 DEST=terraform.app/Contents/MacOS
-cp ../target/release/topo $DEST/terraform
-cp -r ../assets   terraform.app/Contents/MacOS/
+cp ../target/release/topo $DEST/Terraform
+cp -r ../assets   Terraform.app/Contents/MacOS/
 # git rev-parse HEAD > qmoji.app/Contents/MacOS/assets/git-head
-zip -r terraform.zip terraform.app
+zip -r Terraform.app.zip Terraform.app
